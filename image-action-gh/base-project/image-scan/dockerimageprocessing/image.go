33fbd131459e0a3b82fbcc65c4d5ff077ad8c5e4
JayGhiya <ghiya6548@gmail.com>
2024-03-11 16:07:21 +0530

    fix: dummy commit to test tag

--- image-action-gh/base-project/image-scan/dockerimageprocessing/image.go
+++ image-action-gh/base-project/image-scan/dockerimageprocessing/image.go
@@ -28,7 +28,7 @@ func NewImageInfo() *ImageInfo {
 func (ii *ImageInfo) UnmarshalYAML(data []byte) error {
 	var values map[string]interface{}
 	if err := yaml.Unmarshal(data, &values); err != nil {
-		ii.logger.Error("Failed to unmarshal YAML!", zap.Error(err))
+		ii.logger.Error("Failed to unmarshal YAML !", zap.Error(err))
 		return err
 	}
 	ii.findImages(values, "", &ii.Images)
